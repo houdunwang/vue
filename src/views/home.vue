e00b20a60938494716c2378f5af1f827af45f92f
houdunren <2300071698@qq.com>
2022-06-26 16:47:34 +0800

    首页添加访问模块后台按钮

--- src/views/home.vue
+++ src/views/home.vue
@@ -7,10 +7,13 @@ import { isLogin } from '@/utils/helper'
   <div class="h-screen w-screen bg-gray-700 flex flex-col justify-center items-center">
     <img src="/images/xj.jpg" class="w-40 h-40 object-cover rounded-full border-8 border-white mb-5" />
     <div>
-      <FormButtonComponent @click="$router.push({ name: 'site' })" class="danger">后台管理</FormButtonComponent>
-      <FormButtonComponent v-if="!isLogin()" class="primary ml-2" @click="$router.push({ name: RouteEnum.LOGIN })"
-        >用户登录</FormButtonComponent
-      >
+      <FormButtonComponent @click="$router.push({ name: 'site' })" class="danger">系统后台</FormButtonComponent>
+      <FormButtonComponent @click="$router.push({ name: 'admin.index' })" class="primary ml-2">
+        模块后台
+      </FormButtonComponent>
+      <FormButtonComponent class="success ml-2" @click="$router.push({ name: RouteEnum.LOGIN })">
+        用户登录
+      </FormButtonComponent>
     </div>
     <div class="text-gray-300 text-xs font-mono drop-shadow-md mt-6">每晚八点直播，欢迎来共同学习技术</div>
   </div>
