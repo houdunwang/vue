cd6dbb2e620b5e6d707e2929c262632df308564e
houdunren <2300071698@qq.com>
2022-07-11 07:36:20 +0800

    fix:模块跳转

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import CoreAdmin from '#/layouts/admin.vue'
+import siteStore from '#/store/siteStore'
+
+const store = siteStore()
+await store.getCurrentSite()
 </script>
 
 <template>
