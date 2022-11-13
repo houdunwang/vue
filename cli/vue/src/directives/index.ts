472209dc24196438e72ae8e4280a19dd0f72f944
houdunren <2300071698@qq.com>
2022-11-13 13:54:29 +0800

    fix:组件调用方法错误

--- cli/vue/src/directives/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue'
-import clearError from './clearError'
-
-const modules = [clearError]
-
-export default (app: App) => {
-  modules.map((module) => module(app))
-}
--- cli/vue/src/router/modules/index.ts
+++
@@ -1,6 +0,0 @@
-import auth from './auth'
-import front from './front'
-import admin from './admin'
-import error from './error'
-
-export default [...admin, front, auth, error]
