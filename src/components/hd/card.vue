3ed07fdbfe46340f6e3aed54a912dda180b31be0
后盾人 <2300071698@qq.com>
2023-03-21 00:19:17 +0800

    增加组件

---
+++ src/components/hd/card.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="border overflow-hidden shadow-sm rounded-xl bg-white">
+    <h1 class="p-5 py-3 opacity-90 text-lg font-normal border-b" v-if="$slots.header">
+      <slot name="header" />
+    </h1>
+    <div class="p-5">
+      <slot />
+    </div>
+    <div class="px-5 py-3 bg-gray-50 border-t gap-2" v-if="$slots.footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
--- src/layouts/front/index.vue
+++ src/layouts/front/index.vue
@@ -1,4 +1,6 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import footerVue from './footer.vue'
+</script>
 <template>
   <main>
     <RouterView v-slot="{ Component, route }">
@@ -6,6 +8,6 @@
         <component :is="Component" :key="route.fullPath" class="w-full 2xl:w-page mx-auto p-3 lg:mt-5" />
       </template>
     </RouterView>
-    <footerCopyright />
+    <footerVue />
   </main>
 </template>
